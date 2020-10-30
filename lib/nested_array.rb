5cd59d6af9fb92e80056b46e89dd0193c9839454
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-30 17:21:44 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,7 +29,7 @@ def sorted_matrix
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
   matrix = assembled_matrix
-  sort = matrix[0..1].sort
+  sorted = matrix[0..1].sort
   # sorted = []
   # size = matrix.size
   # size.times do |x|
